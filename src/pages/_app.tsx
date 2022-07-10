c9e2de7fe1365cd5ed2a559f4946bc6bdb129fb7
Dz0526 <this@dz99.me>
2022-07-10 14:07:18 +0900

    Feat: DarkMode に対応

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react';
+import theme from 'theme/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <ColorModeProvider>
+        <Component {...pageProps} />;
+      </ColorModeProvider>
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
